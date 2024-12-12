cb19e3ab837a5bf59e94dd28bb8026b8b28b2757
Vidhi Agrawal <163636860+vidhiagrawa@users.noreply.github.com>
2024-12-12 17:04:11 +0000

    adding

--- conditionPYOs/que2.java
+++ conditionPYOs/que2.java
@@ -1,27 +1,27 @@
-// Take two int values from user and print greatest among them.
+// // Take two int values from user and print greatest among them.
 
 
-package conditionPYOs;
+// package conditionPYOs;
 
-import java.util.*;
+// import java.util.*;
 
-public class que2 {
-    public static void main( String[] args ){
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Give the value of A :- ");
-        int a = sc.nextInt();
-        System.out.println("Give the value of B :- ");
-        int b = sc.nextInt();
+// public class que2 {
+//     public static void main( String[] args ){
+//         Scanner sc = new Scanner(System.in);
+//         System.out.println("Give the value of A :- ");
+//         int a = sc.nextInt();
+//         System.out.println("Give the value of B :- ");
+//         int b = sc.nextInt();
 
-        if( a > b ){
-            System.out.println("A is greater than B ");
-        }
-        else if ( b > a ){
-            System.out.println("B is greater than A ");
-        }
-        else{
-            System.out.println("A is equal to B ");
+//         if( a > b ){
+//             System.out.println("A is greater than B ");
+//         }
+//         else if ( b > a ){
+//             System.out.println("B is greater than A ");
+//         }
+//         else{
+//             System.out.println("A is equal to B ");
 
-        }
-    }
-}
+//         }
+//     }
+// }
